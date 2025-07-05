dae1a7f35de382db8432ebb520b742a24c518606
Simon Zhu <simonsiyuanzhu@gmail.com>
2025-07-05 18:16:50 +0100

    dcsr_prv reset to M-mode

--- vendor/thead_openc910/C910_RTL_FACTORY/gen_rtl/cp0/rtl/ct_cp0_regs.v
+++ vendor/thead_openc910/C910_RTL_FACTORY/gen_rtl/cp0/rtl/ct_cp0_regs.v
@@ -3958,11 +3958,7 @@ begin
   if(!cpurst_b) begin
     dcsr_cause     <= 3'b0;
     dcsr_v         <= 1'b0;
-<<<<<<< HEAD
     dcsr_prv       <= 2'b11; // M-Mode
-=======
-    dcsr_prv       <= 2'b11; //M-Mode
->>>>>>> e867ce6bd7156b4a8de2dcafa5b49678dd9629bb
   end else if(dcsr_local_en) begin
     dcsr_cause     <= iui_regs_src0[8:6];
     dcsr_v         <= iui_regs_src0[5];
